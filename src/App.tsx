5bc92dcf80d933116fb4ed04fceaaf74493e74ba
oliullah05 <mdolihasan@gmail.com>
2024-01-12 17:23:03 +0600

    increment , decrement done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment } from "./redux/features/countSlice"
 
 
 function App() {
 
 const count = useSelector((state)=>state.counter.value)
+
+const dispatch = useDispatch()
+
   return (
     <>
         <div className="flex justify-center items-center h-screen ">
       <button  className="px-4 py-4 bg-green-500 rounded-lg mx-5">increment By 5</button>
-      <button  className="px-4 py-4 bg-green-500 rounded-lg mx-5">increment</button>
+      <button  onClick={()=>dispatch(increment())} className="px-4 py-4 bg-green-500 rounded-lg mx-5">increment</button>
       <h1 className="text-3xl ">{count}</h1>
-      <button  className="px-4 py-4 bg-green-500 rounded-lg mx-5">decrement</button>
+      <button onClick={()=>dispatch(decrement())} className="px-4 py-4 bg-green-500 rounded-lg mx-5">decrement</button>
     </div>
     </>
   )
